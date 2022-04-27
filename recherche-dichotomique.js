a34352a05a7948c7b6961acf3b18ab73f6bf7028
sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2022-04-27 22:23:32 +0300

    méthode pour faire une recherche plus rapide

---
+++ recherche-dichotomique.js
@@ -0,0 +1,7 @@
+function rechdicho(elt,tab,debut,fin){
+    let m;
+    m = Math.floor((debut+fin)/2);
+    if(debut > fin){
+        return `le nombrbe que vous avez taper n'est pas dans le tableau`;
+    }
+}
