6088c5593ee75ac65a97a0c6ee18d355b411a80c
sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2022-04-28 21:33:38 +0300

    1ère modification

--- recherche-dichotomique.js
+++ recherche-dichotomique.js
@@ -4,4 +4,7 @@ function rechdicho(elt,tab,debut,fin){
     if(debut > fin){
         return `le nombrbe que vous avez taper n'est pas dans le tableau`;
     }
+    else if(debut < fin){
+        
+    }
 }
