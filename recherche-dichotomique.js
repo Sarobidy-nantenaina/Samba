1c9d2949a2d708b5034642cc9961c858d7ccd327
sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2022-04-29 18:56:42 +0300

    suite du code recherche dichotomique

--- recherche-dichotomique.js
+++ recherche-dichotomique.js
@@ -4,7 +4,15 @@ function rechdicho(elt,tab,debut,fin){
     if(debut > fin){
         return `le nombrbe que vous avez taper n'est pas dans le tableau`;
     }
-    else if(debut < fin){
-        
+    else if(debut<=fin){
+       if(tab[m] == elt){
+          return `le nombre que vous avez taper est bien dans le tableau`;
+       }
+        if(tab[m] < elt){
+          return rechdicho(elt,tab,(m+1),fin);
+       }
+       if(tab[m] > elt){
+          return rechdicho(elt,tab,debut,(m-1));
+       }
     }
 }
